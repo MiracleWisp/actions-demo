851db23a98fd89abeb013d9bedb31bb804f6181c
Semyon Uchvatov <s.uchvatov@tinkoff.ru>
2024-01-30 20:28:45 +0300

    Docker push

--- src/main/kotlin/com/example/actionsdemo/ActionsDemoApplication.kt
+++ src/main/kotlin/com/example/actionsdemo/ActionsDemoApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class ActionsDemoApplication
 
+
 fun main(args: Array<String>) {
     runApplication<ActionsDemoApplication>(*args)
 }
