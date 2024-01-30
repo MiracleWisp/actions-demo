b1853f8b4354497ce5e20946f8fa317915818324
Semyon Uchvatov <s.uchvatov@tinkoff.ru>
2024-01-30 17:29:37 +0300

    Init

---
+++ src/main/kotlin/com/example/actionsdemo/ActionsDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.actionsdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActionsDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ActionsDemoApplication>(*args)
+}
